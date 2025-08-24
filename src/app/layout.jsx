217e0fe89e30277ba8fc9bb85a3f4297da171388
ian <maceachen16@gmail.com>
2025-08-24 17:35:33 -0300

    add: favicon

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -30,7 +30,7 @@ export const metadata = {
   metadataBase, */
   title: info.title,
   icons: {
-    icon: "/assets/favicon.ico"
+    icon: "/assets/growly.ico"
   },
   description: info.description,
   keywords: [
