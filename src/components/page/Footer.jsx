68f9e12ac9d32cd69199bd44596daadcdc65f9e7
ian <maceachen16@gmail.com>
2025-08-24 22:18:05 -0300

    chore: minor changes

--- src/components/page/Footer.jsx
+++ src/components/page/Footer.jsx
@@ -7,16 +7,16 @@ export default function Footer(){
             <img src="assets/growly.webp" className="absolute h-24 mx-5" alt="Growly Logo" />
             <ul className="flex justify-center m-5 text-sm font-semibold text-black sm:mb-0 dark:text-gray-400">
                 <li>
-                    <a href="#" className="hover:underline me-4 md:me-6">About</a>
+                    <a href="#" className="inline-block hover:text-gray-500 me-4 md:me-6">About</a>
                 </li>
                 <li>
-                    <a href="#" className="hover:underline me-4 md:me-6">Privacy Policy</a>
+                    <a href="#" className="inline-block hover:text-gray-500 me-4 md:me-6">Privacy Policy</a>
                 </li>
                 <li>
-                    <a href="#" className="hover:underline me-4 md:me-6">Licensing</a>
+                    <a href="#" className="inline-block hover:text-gray-500 me-4 md:me-6">Licensing</a>
                 </li>
                 <li>
-                    <a href="#" className="hover:underline">Contact</a>
+                    <a href="#" className="inline-block hover:text-gray-500">Contact</a>
                 </li>
             </ul>
             <p className="my-3 text-black text-center font-semibold">&copy; {new Date().getFullYear()} Growly. All rights reserved.</p>
--- src/components/page/Navbar.jsx
+++ src/components/page/Navbar.jsx
@@ -6,7 +6,7 @@ import ActionText from "@ui/ActionText";
 export default function Navbar(){
     return (
         <header>
-          <nav className="flex fixed top-0 justify-between rounded-full items-center bg-growly-white text-black font-bold p-4 mb-6 h-16 w-4/4 mx-auto">
+          <nav className="flex fixed top-0 justify-between items-center border-b-1 border-gray-200 bg-growly-white text-black font-bold p-4 mb-6 h-16 w-4/4 z-20 mx-auto">
             <img src="assets/growly.webp" alt="" className="h-fit w-1/6"/>
             <div className="flex items-center">
               <ul className="flex pr-5">
@@ -23,6 +23,7 @@ export default function Navbar(){
             </ul>
             </div>
           </nav>
+    
         </header>
     );
 }
